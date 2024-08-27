2173b3db1f94c7f3a70cd2dcc2a5fea42ea93490
joão vitor <joaovitorjv200818@gmail.com>
2024-08-27 19:27:32 -0300

    Alterado título

--- back/cadastro_usuario.php
+++ back/cadastro_usuario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Cliente</title>
+    <title>Cadastro de Clientes</title>
 </head>
 <body>
     <h2>Cadastro de Cliente</h2>
